e7aaca9a00284f6c4fea3bfd43c7e8006d8fddc7
ermias kidane <ermiaskidanegebre@gmail.com>
2023-06-09 23:04:28 +0100

    cloudinary upload

--- app/components/Modals/RentModal.tsx
+++ app/components/Modals/RentModal.tsx
@@ -14,11 +14,11 @@ import { useMemo, useState } from "react";
 import useRentModal from '@/app/hooks/useRentModal';
 
 import Modal from "./Modal";
-// import Counter from "../inputs/Counter";
+import Counter from "../inputs/Counter";
 import CategoryInput from '../inputs/CategoryInput';
 import CountrySelect from "../inputs/CountrySelect";
 import { categories } from '../navbar/Categories';
-// import ImageUpload from '../inputs/ImageUpload';
+import ImageUpload from '../inputs/ImageUpload';
 import Input from '../inputs/Input';
 import Heading from '../Heading';
 // import Map from "../Map"
@@ -185,51 +185,51 @@ const RentModal = () => {
     );
   }
 
-  // if (step === STEPS.INFO) {
-  //   bodyContent = (
-  //     <div className="flex flex-col gap-8">
-  //       <Heading
-  //         title="Share some basics about your place"
-  //         subtitle="What amenitis do you have?"
-  //       />
-  //       <Counter 
-  //         onChange={(value) => setCustomValue('guestCount', value)}
-  //         value={guestCount}
-  //         title="Guests" 
-  //         subtitle="How many guests do you allow?"
-  //       />
-  //       <hr />
-  //       <Counter 
-  //         onChange={(value) => setCustomValue('roomCount', value)}
-  //         value={roomCount}
-  //         title="Rooms" 
-  //         subtitle="How many rooms do you have?"
-  //       />
-  //       <hr />
-  //       <Counter 
-  //         onChange={(value) => setCustomValue('bathroomCount', value)}
-  //         value={bathroomCount}
-  //         title="Bathrooms" 
-  //         subtitle="How many bathrooms do you have?"
-  //       />
-  //     </div>
-  //   )
-  // }
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Share some basics about your place"
+          subtitle="What amenitis do you have?"
+        />
+        <Counter 
+          onChange={(value) => setCustomValue('guestCount', value)}
+          value={guestCount}
+          title="Guests" 
+          subtitle="How many guests do you allow?"
+        />
+        <hr />
+        <Counter 
+          onChange={(value) => setCustomValue('roomCount', value)}
+          value={roomCount}
+          title="Rooms" 
+          subtitle="How many rooms do you have?"
+        />
+        <hr />
+        <Counter 
+          onChange={(value) => setCustomValue('bathroomCount', value)}
+          value={bathroomCount}
+          title="Bathrooms" 
+          subtitle="How many bathrooms do you have?"
+        />
+      </div>
+    )
+  }
 
-  // if (step === STEPS.IMAGES) {
-  //   bodyContent = (
-  //     <div className="flex flex-col gap-8">
-  //       <Heading
-  //         title="Add a photo of your place"
-  //         subtitle="Show guests what your place looks like!"
-  //       />
-  //       <ImageUpload
-  //         onChange={(value) => setCustomValue('imageSrc', value)}
-  //         value={imageSrc}
-  //       />
-  //     </div>
-  //   )
-  // }
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Add a photo of your place"
+          subtitle="Show guests what your place looks like!"
+        />
+        <ImageUpload
+          onChange={(value) => setCustomValue('imageSrc', value)}
+          value={imageSrc}
+        />
+      </div>
+    )
+  }
 
   // if (step === STEPS.DESCRIPTION) {
   //   bodyContent = (
